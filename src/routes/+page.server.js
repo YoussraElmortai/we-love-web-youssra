e21d5b5e8f437d4075afd5274729d19f9e4bcb74
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-10-12 09:32:04 +0200

    ga met hygraph verder

--- src/routes/+page.server.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '$lib/prismicio';
-
-export async function load({ fetch, request }) {
-	const client = createClient({ fetch, request });
-	const page = await client.getByUID('index', 'index');
-	console.log(page.data);
-	return page.data;
-}
