6aa5adb44ca279695b00d9c2850b8cfeb72bba55
youssra <youssramortai01@gmail.com>
2023-12-01 14:46:11 +0100

    navigation basic html/css :fish:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
 <script></script>
 
+<nav><img src="Vectorbluespiderweb.png" alt="We love web website"> <a href="/">HOME</a></nav>
+
+<style>
+    nav{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        height: 5rem;
+    }
+
+    nav a{
+        font-size: 22px;
+        font-weight: bold;
+        margin: 2rem;
+        text-decoration: none;
+        color: var(--text-color);
+    }
+
+
+</style>
