20263cee45b3af8dbd2c50c73d43ec98e6ea25b7
youssra <youssramortai01@gmail.com>
2023-12-04 12:44:59 +0100

    styling en accordions erin :fish:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,20 +2,79 @@
 
 <nav><img src="Vectorbluespiderweb.png" alt="We love web website"> <a href="/">HOME</a></nav>
 
+<section class="accordion">
+    <span>Workshops</span>
+    <span>Workshops</span>
+    <span>Workshops</span>
+    <span>Workshops</span>
+    <span>Workshops</span>
+</section>
+<section class="accordion">
+    <span>Workshops</span>
+    <span>Workshops</span>
+    <span>Workshops</span>
+    <span>Workshops</span>
+    <span>Workshops</span>
+</section>
+<section class="accordion">
+    <span>Workshops</span>
+    <span>Workshops</span>
+    <span>Workshops</span>
+    <span>Workshops</span>
+    <span>Workshops</span>
+</section>
+
+
+
+
 <style>
+
+    .accordion{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        height: 10vh;
+        width: 100%;
+        border-top: solid 2px var(--accent-color);
+        transition: 0.5s ease-in-out;
+        overflow: hidden;
+    }
+
+    .accordion:hover{
+        background-color: var(--accent-color);
+    }
+
+    .accordion span{
+        font-size: 30px;
+        position: relative;
+        top: 0.3rem;
+        font-family: var(--accent-font)
+    }
+
     nav{
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         height: 5rem;
+        margin-bottom: 1rem;
+    }
+
+    nav img{
+        width: auto;
+        height: 4rem;
+        margin: 0.3rem;
     }
 
     nav a{
         font-size: 22px;
+        margin:1.5rem;
         font-weight: bold;
-        margin: 2rem;
         text-decoration: none;
         color: var(--text-color);
+    }
+
+    nav a:hover{
+        border-bottom: 2px solid var(--accent-color);
     }
 
 
